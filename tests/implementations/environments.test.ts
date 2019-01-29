59e07de7923695f3b855b226c509c1bdcd003584
MikeSquall <michael.arbib@om2.com>
2019-01-29 16:19:22 +0200

    Init tests

---
+++ tests/implementations/environments.test.ts
@@ -0,0 +1,5 @@
+describe('Register test suite', () => {
+  it('Subscribe to environments$ method returns all available envKeys and Envs', () => {});
+
+  it('Server error occurs when subscribing to environments$', () => {});
+});
---
+++ tests/implementations/register.test.ts
@@ -0,0 +1,9 @@
+describe('Register test suite', () => {
+  it('Calling register method registers the version of the provided envKey and Env ', () => {});
+
+  it('Calling register method without providing the envKey and Env (empty envKey or Env) ', () => {});
+
+  it('Calling register method with an envKey already registered ', () => {});
+
+  it('Server error occurs after calling register method ', () => {});
+});
