49c0468ba1c94152f34fa8d76df063d21bc37ee2
MikeSquall <michael.arbib@om2.com>
2019-01-29 12:14:08 +0200

    Added docs

--- src/api/Env.ts
+++ src/api/Env.ts
@@ -1,5 +1,7 @@
 export interface Env {
+  /** Env access points is an Array of url */
   accessPoints: {
+    /** The url gives access to a specific version of a project */
     url: string;
   }[];
 }
