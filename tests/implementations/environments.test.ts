77f7a5a2d0c97ffe37a47b6d2ac42cb754c536be
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-02-06 10:11:51 +0200

    fix

--- tests/implementations/environments.test.ts
+++ tests/implementations/environments.test.ts
@@ -2,7 +2,7 @@ import EnvRegistry from '../../src/EnvRegistry';
 import {
   ConfigurationServiceLocalStorage
 } from '@capsulajs/capsulajs-configuration-service';
-import { configurationServiceResponseMock } from './mocks';
+import { environments } from './mocks';
 
 describe('Register test suite', () => {
   const configurationService = new ConfigurationServiceLocalStorage('token');
@@ -12,8 +12,10 @@ describe('Register test suite', () => {
   beforeEach(async () => {
     localStorage.clear();
     await configurationService.createRepository({repository});
-    await configurationService.save({repository, key: 'test', value: {}});
-    await configurationService.save({repository, key: 'develop', value: {}});
+    await configurationService.save({repository, key: 'develop', value: environments.develop});
+    await configurationService.save({repository, key: 'master', value: environments.master});
+    await configurationService.save({repository, key: 'tag-1', value: environments['tag-1']});
+    await configurationService.save({repository, key: 'tag-2', value: environments['tag-2']});
   });
 
   it('Subscribe to environments$ method returns all available envKeys and Envs', async () => {
--- tests/implementations/mocks.ts
+++ tests/implementations/mocks.ts
@@ -1,35 +1,26 @@
-export const configurationServiceResponseMock = [
-  {
-    key: 'develop',
-    value: {
-      accessPoints: [
-        { url: 'http://accessPoint/dev/service1'},
-        { url: 'http://accessPoint/dev/service2'}
-      ]
-    }},
-  {
-    key: 'master',
-    value: {
-      accessPoints: [
-        { url: 'http://accessPoint/master/service1'},
-        { url: 'http://accessPoint/master/service2'}
-      ]
-    }},
-  {
-    key: 'tag-1',
-    value: {
-      accessPoints: [
-        { url: 'http://accessPoint/tag-1/service1'},
-        { url: 'http://accessPoint/tag-1/service2'}
-      ]
-    }},
-  {
-    key: 'tag-2',
-    value: {
-      accessPoints: [
-        { url: 'http://accessPoint/tag-2/service1'},
-        { url: 'http://accessPoint/tag-2/service2'}
-      ]
-    }
+export const environments: any = {
+  develop: {
+    accessPoints: [
+      {url: 'http://accessPoint/dev/service1'},
+      {url: 'http://accessPoint/dev/service2'}
+    ]
+  },
+  master: {
+    accessPoints: [
+      {url: 'http://accessPoint/master/service1'},
+      {url: 'http://accessPoint/master/service2'}
+    ]
+  },
+  'tag-1': {
+    accessPoints: [
+      {url: 'http://accessPoint/tag-1/service1'},
+      {url: 'http://accessPoint/tag-1/service2'}
+    ]
+  },
+  'tag-2': {
+    accessPoints: [
+      {url: 'http://accessPoint/tag-2/service1'},
+      {url: 'http://accessPoint/tag-2/service2'}
+    ]
   }
-];
+};
