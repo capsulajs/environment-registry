5c1bb5c76ae751e2aa6be496033ecb6ddb5ee3da
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-02-06 08:47:35 +0200

    add mocks

--- tests/implementations/environments.test.ts
+++ tests/implementations/environments.test.ts
@@ -2,39 +2,10 @@ import EnvRegistry from '../../src/EnvRegistry';
 import {
   ConfigurationServiceLocalStorage
 } from '@capsulajs/capsulajs-configuration-service';
+import { configurationServiceResponseMock } from './mocks';
 
 describe('Register test suite', () => {
   // TODO import configuration service and mock confService.entries()
-  const configurationServiceResponseMock = [
-    {
-      key: 'develop',
-      value: {
-        accessPoints: [
-          { url: 'http://accessPoint/dev/service1'},
-          { url: 'http://accessPoint/dev/service2'}
-        ]}},
-    {
-      key: 'master',
-      value: {
-        accessPoints: [
-          { url: 'http://accessPoint/master/service1'},
-          { url: 'http://accessPoint/master/service2'}
-        ]}},
-    {
-      key: 'tag-1',
-      value: {
-        accessPoints: [
-          { url: 'http://accessPoint/tag-1/service1'},
-          { url: 'http://accessPoint/tag-1/service2'}
-        ]}},
-    {
-      key: 'tag-2',
-      value: {
-        accessPoints: [
-          { url: 'http://accessPoint/tag-2/service1'},
-          { url: 'http://accessPoint/tag-2/service2'}
-        ]}},
-  ];
   const configurationService = new ConfigurationServiceLocalStorage('token');
   const repository = 'environmentRegistry';
 
---
+++ tests/implementations/mocks.ts
@@ -0,0 +1,33 @@
+export const configurationServiceResponseMock = [
+  {
+    key: 'develop',
+    value: {
+      accessPoints: [
+        { url: 'http://accessPoint/dev/service1'},
+        { url: 'http://accessPoint/dev/service2'}
+      ]}},
+  {
+    key: 'master',
+    value: {
+      accessPoints: [
+        { url: 'http://accessPoint/master/service1'},
+        { url: 'http://accessPoint/master/service2'}
+      ]}},
+  {
+    key: 'tag-1',
+    value: {
+      accessPoints: [
+        { url: 'http://accessPoint/tag-1/service1'},
+        { url: 'http://accessPoint/tag-1/service2'}
+      ]
+    }},
+  {
+    key: 'tag-2',
+    value: {
+      accessPoints: [
+        { url: 'http://accessPoint/tag-2/service1'},
+        { url: 'http://accessPoint/tag-2/service2'}
+      ]
+    }
+  }
+];
