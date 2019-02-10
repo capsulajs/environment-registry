6c85952032032ec3adc7df04d1f9f7cf8f702547
MikeSquall <michael.arbib@om2.com>
2019-02-10 17:27:43 +0200

    Refactored test

--- tests/implementations/register.test.ts
+++ tests/implementations/register.test.ts
@@ -5,11 +5,6 @@ import { validationMessages } from '../../src/utils';
 describe('Register test suite', () => {
   let envRegistry: EnvRegistry;
 
-  const checkExpectedEntries = async (expected: any) => {
-    const entries = await envRegistry.configurationService.entries({ repository: 'environmentRegistry'});
-    await expect(entries).toEqual(expected);
-  };
-
   beforeEach(() => {
     localStorage.clear();
     envRegistry = new EnvRegistry('token');
@@ -25,18 +20,19 @@ describe('Register test suite', () => {
   });
 
   it('Calling register method without providing the envKey and Env (empty envKey or Env) ', async () => {
-    expect.assertions(14);
+    expect.assertions(16);
 
-    const badValues = [null, undefined, 123, [], ['test'], {}, { test: 'test' }];
-    badValues.forEach((envKey) => {
+    const badEnvKeyValues = [null, undefined, 123, [], ['test'], {}, { test: 'test' }];
+    badEnvKeyValues.forEach((envKey) => {
       // @ts-ignore
       envRegistry.register({ envKey, env: environments.develop })
         .catch((err: any) => expect(err).toEqual(new Error(validationMessages.envKeyIsNotCorrect)));
     });
 
-    badValues.forEach((envKey) => {
+    const badEnvValues = [null, undefined, 123, 'test', [], ['test'], {}, { test: 'test' }, { accessPoints: [] }];
+    badEnvValues.forEach((env) => {
       // @ts-ignore
-      envRegistry.register({ envKey: 'master', env: {} })
+      envRegistry.register({ envKey: 'master', env })
         .catch((err: any) => expect(err).toEqual(new Error(validationMessages.envIsNotCorrect)));
     });
   });
@@ -44,18 +40,14 @@ describe('Register test suite', () => {
   it('Calling register method with an envKey already registered ', async (done) => {
     expect.assertions(1);
     await envRegistry.register({ envKey: 'test', env: environments.develop});
-    // envRegistry.environments$({}).subscribe((env) => {
-    //   console.log(env);
-    //   expect(env).toEqual({});
-    // });
-    envRegistry.register({ envKey: 'test', env: environments.master })
-      .then(async () => {
-        await checkExpectedEntries({ entries: [{ key: 'test', value: { accessPoints: [
-                { url: 'http://accessPoint/master/service1' },
-                { url: 'http://accessPoint/master/service2' }
-              ]}}]});
-        done();
-      });
+    await envRegistry.register({ envKey: 'test', env: environments.master });
+    envRegistry.environments$({}).subscribe((env) => {
+      expect(env).toEqual({ key: 'test', value: { accessPoints: [
+            { url: 'http://accessPoint/master/service1' },
+            { url: 'http://accessPoint/master/service2' }
+          ]}});
+      done();
+    });
   });
 
   // it('Server error occurs after calling register method ', () => {});
