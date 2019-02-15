c363a90ec06d90eeb6c220bc2abff60168129cd1
dmitriyslaym <dmitriyslaym@gmail.com>
2019-02-15 11:13:28 +0200

    Added linting

--- src/api/Env.ts
+++ src/api/Env.ts
@@ -1,7 +1,7 @@
 export interface Env {
   /** Env access points is an Array of url */
-  accessPoints: {
+  accessPoints: Array<{
     /** The url gives access to a specific version of a project */
     url: string;
-  }[];
+  }>;
 }
