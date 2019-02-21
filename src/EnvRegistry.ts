7d118bd4fc25c2141e9ece4e924c2dd12567a6bf
Michael Arbib <michael.arbib@om2.com>
2019-02-21 09:45:46 +0200

    Tests & implementation (#4)
    * Init tests
    
    * wip
    
    * Setup jest config
    
    * WIP on test + environments$ method
    
    * add @types/node
    
    * add mocks
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * fix
    
    * refactroring
    
    * wip
    
    * Fixed register method
    
    * Added requestValidator + fix environments
    
    * Enable Travis to run tests
    
    * fix codacy issues in jest config
    
    * Added environments$ test
    
    * Refactored test
    
    * Clean code
    
    * Updated tests
    
    * Added codacy coverage
    
    * Rewrite the environments$ test to make sure that order has no impact
    
    * Upgraded rxjs version
    
    * Included tests in tsconfig
    
    * Changed following review
    
    * Removed unused comments
    
    * Fixed implicit any type + fixed tests
    
    * Small fixes
    
    * Fixed yarn.lock
    
    * Fixed linting errors
    
    * Set createRepo method private
    
    * Upgraded conf service package
    
    * Renaming validator functions

---
+++ src/EnvRegistry.ts
@@ -0,0 +1,63 @@
+import { from } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import {
+  ConfigurationService,
+  ConfigurationServiceLocalStorage,
+  messages,
+} from '@capsulajs/capsulajs-configuration-service';
+import {
+  EnvRegistry as EnvRegistryInterface,
+  EnvRegistryItem,
+  EnvironmentsRequest,
+  EnvironmentsResponse,
+  RegisterResponse,
+} from './api/EnvRegistry';
+import { isEnvKeyValid, isEnvValid, validationMessages } from './utils';
+import { ConfigEntry, EntriesResponse } from './types';
+
+export default class EnvRegistry implements EnvRegistryInterface {
+  private configurationService: ConfigurationService;
+  private repositoryCreated: boolean;
+  private readonly repository: string;
+
+  constructor(token: string) {
+    this.configurationService = new ConfigurationServiceLocalStorage(token); // add logic to choose specific provider
+    this.repositoryCreated = false;
+    this.repository = 'environmentRegistry';
+  }
+
+  public async register(registerRequest: EnvRegistryItem): Promise<RegisterResponse> {
+    if (!isEnvValid(registerRequest)) {
+      return Promise.reject(new Error(validationMessages.envIsNotCorrect));
+    }
+    if (!isEnvKeyValid(registerRequest)) {
+      return Promise.reject(new Error(validationMessages.envKeyIsNotCorrect));
+    }
+
+    if (!this.repositoryCreated) {
+      try {
+        await this.createRepository();
+        this.repositoryCreated = true;
+      } catch (e) {
+        e.message === messages.repositoryAlreadyExists ? (this.repositoryCreated = true) : new Error(e);
+      }
+    }
+    return this.save({ key: registerRequest.envKey, value: registerRequest.env });
+  }
+
+  public environments$(environmentsRequest: EnvironmentsRequest): EnvironmentsResponse {
+    return from(this.configurationService.entries({ repository: this.repository })).pipe(
+      switchMap((response: EntriesResponse) => {
+        return from(response.entries.map((entry) => ({ envKey: entry.key, env: entry.value })));
+      })
+    );
+  }
+
+  private save(params: ConfigEntry) {
+    return this.configurationService.save({ repository: this.repository, key: params.key, value: params.value });
+  }
+
+  private createRepository() {
+    return this.configurationService.createRepository({ repository: this.repository });
+  }
+}
---
+++ tests/implementations/environments.test.ts
@@ -0,0 +1,53 @@
+import EnvRegistry from '../../src/EnvRegistry';
+import { environments } from '../helpers/mocks';
+import { EnvRegistryItem } from '../../src/api/EnvRegistry';
+
+describe('Environments$ test suite', () => {
+  let envRegistry: EnvRegistry;
+
+  beforeEach(async () => {
+    localStorage.clear();
+    envRegistry = new EnvRegistry('token');
+    await envRegistry.register({ envKey: 'develop', env: environments.develop });
+    await envRegistry.register({ envKey: 'master', env: environments.master });
+    await envRegistry.register({ envKey: 'tag-1', env: environments['tag-1'] });
+  });
+
+  it('Subscribe to environments$ method returns all available envKeys and Envs', async (done) => {
+    expect.assertions(4);
+    const receivedEnvs: EnvRegistryItem[] = [];
+    const expectedArray = [
+      { envKey: 'tag-1', env: environments['tag-1'] },
+      { envKey: 'master', env: environments.master },
+      { envKey: 'develop', env: environments.develop },
+    ];
+    const expectedUpdatedArray = [
+      { envKey: 'tag-1', env: environments['tag-1'] },
+      { envKey: 'master', env: environments.master },
+      { envKey: 'tag-2', env: environments['tag-2'] },
+      { envKey: 'develop', env: environments.develop },
+    ];
+    await new Promise((resolve) => {
+      envRegistry.environments$({}).subscribe(
+        (data) => receivedEnvs.push(data),
+        (err) => new Error(err),
+        () => {
+          expect(receivedEnvs).toHaveLength(3);
+          expect(receivedEnvs).toEqual(expect.arrayContaining(expectedArray));
+          resolve();
+        }
+      );
+    });
+    await envRegistry.register({ envKey: 'tag-2', env: environments['tag-2'] });
+    receivedEnvs.length = 0;
+    envRegistry.environments$({}).subscribe(
+      (data) => receivedEnvs.push(data),
+      (err) => new Error(err),
+      () => {
+        expect(receivedEnvs).toHaveLength(4);
+        expect(receivedEnvs).toEqual(expect.arrayContaining(expectedUpdatedArray));
+        done();
+      }
+    );
+  });
+});
