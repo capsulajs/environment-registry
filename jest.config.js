82829453862b7945f24b9b7390896a3de3610dff
Michael Arbib <michael.arbib@om2.com>
2019-04-02 12:01:14 +0300

    Update README (#9)
    * Updated readme
    
    * Updated readme
    
    * Updated readme
    
    * Fix Codacy
    
    * Updated readme
    
    * Updated README
    
    * Added remark linter
    
    * Ignore .md files in prettier

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,10 @@
 module.exports = {
-  setupFiles: [
-    "jest-localstorage-mock"
-  ],
+  setupFiles: ['jest-localstorage-mock'],
   transform: {
-    "\\.ts$": "ts-jest"
+    '\\.ts$': 'ts-jest',
   },
-  testRegex: ".*\\.test\\.ts$",
-  moduleFileExtensions: [
-    "ts",
-    "js"
-  ],
-  moduleDirectories: [
-    "src",
-    "node_modules"
-  ],
-  testEnvironment: "node"
+  testRegex: '.*\\.test\\.ts$',
+  moduleFileExtensions: ['ts', 'js'],
+  moduleDirectories: ['src', 'node_modules'],
+  testEnvironment: 'node',
 };
