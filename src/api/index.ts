b4c6b6583592457c52d23c60b5486c88478dc9f1
Michael Arbib <michael.arbib@om2.com>
2019-04-11 15:06:38 +0300

    Expose interfaces (#15)

---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+export { Env, EnvService, EnvServiceMethod, EnvServiceMethods, AsyncModelType } from './Env';
+
+export {
+  EnvRegistry,
+  EnvironmentsRequest,
+  EnvironmentsResponse,
+  EnvRegistryItem,
+  RegisterResponse,
+} from './EnvRegistry';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 export { EnvRegistry } from './EnvRegistry';
+export {
+  Env,
+  EnvService,
+  EnvServiceMethod,
+  EnvServiceMethods,
+  AsyncModelType,
+  EnvRegistry as EnvRegistryInterface,
+  EnvRegistryItem,
+  EnvironmentsRequest,
+  EnvironmentsResponse,
+  RegisterResponse,
+} from './api';
