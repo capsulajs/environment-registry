e5939e90787436d39f33d1c738b6423414677a0b
dmitriyslaym <dmitriyslaym@gmail.com>
2019-08-20 16:24:29 +0300

    Fix CI/CD for develop

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -4,5 +4,5 @@ export const ENV_REGISTRY_ITEM_PROPS_LENGTH = 2;
 export const validationMessages = {
   registerRequestIsNotCorrect:
     'registerRequest has not provided or is not matching this pattern: { envKey: string, env: any }',
-  envKeyIsNotCorrect: 'envKey has not been provided or is not a string',
+  envKeyIsNotCorrect: 'envKey has not been provided or it is not a string',
 };
