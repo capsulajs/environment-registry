5797f45975b7618a6de7f566596477717642714a
dmitriyslaym <dmitriyslaym@gmail.com>
2019-08-20 18:35:27 +0300

    Fix npm version update

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -3,6 +3,6 @@ export const ENV_REGISTRY_ITEM_PROPS_LENGTH = 2;
 
 export const validationMessages = {
   registerRequestIsNotCorrect:
-    'registerRequest has not been provided or is not matching this pattern: { envKey: string, env: any }',
+    'registerRequest has not been provided or does not match this pattern: { envKey: string, env: any }',
   envKeyIsNotCorrect: 'envKey has not been provided or it is not a string',
 };
