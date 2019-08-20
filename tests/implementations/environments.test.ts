29560914f0731c1190db7570a670b307eb93d231
dmitriyslaym <dmitriyslaym@gmail.com>
2019-08-20 13:30:47 +0300

    HttpFile configProvider check environments$

--- tests/implementations/environments.test.ts
+++ tests/implementations/environments.test.ts
@@ -1,3 +1,4 @@
+import { toArray } from 'rxjs/operators';
 import { configurationTypes } from '@capsulajs/capsulajs-configuration-service';
 import { EnvRegistry } from '../../src';
 import { environments } from '../helpers/mocks';
@@ -7,28 +8,24 @@ import { EnvRegistryItem } from '../../src/api/EnvRegistry';
 describe('Environments$ test suite', () => {
   let envRegistry: EnvRegistry<Env>;
 
+  const devData = { envKey: 'develop', env: environments.develop };
+  const masterData = { envKey: 'master', env: environments.master };
+  const tag1Data = { envKey: 'tag-1', env: environments['tag-1'] };
+  const tag2Data = { envKey: 'tag-2', env: environments['tag-2'] };
+
   beforeEach(async () => {
     localStorage.clear();
     envRegistry = new EnvRegistry({ token: 'token', configProvider: configurationTypes.localStorage });
-    await envRegistry.register({ envKey: 'develop', env: environments.develop });
-    await envRegistry.register({ envKey: 'master', env: environments.master });
-    await envRegistry.register({ envKey: 'tag-1', env: environments['tag-1'] });
+    await envRegistry.register(devData);
+    await envRegistry.register(masterData);
+    await envRegistry.register(tag1Data);
   });
 
-  it('Subscribe to environments$ method returns all available envKeys and Envs', async (done) => {
+  it('Subscribe to environments$ method returns all available envKeys and Envs (LocalStorage configProvider)', async (done) => {
     expect.assertions(4);
     const receivedEnvs: Array<EnvRegistryItem<Env>> = [];
-    const expectedArray = [
-      { envKey: 'tag-1', env: environments['tag-1'] },
-      { envKey: 'master', env: environments.master },
-      { envKey: 'develop', env: environments.develop },
-    ];
-    const expectedUpdatedArray = [
-      { envKey: 'tag-1', env: environments['tag-1'] },
-      { envKey: 'master', env: environments.master },
-      { envKey: 'tag-2', env: environments['tag-2'] },
-      { envKey: 'develop', env: environments.develop },
-    ];
+    const expectedArray = [tag1Data, masterData, devData];
+    const expectedUpdatedArray = [tag1Data, masterData, tag2Data, devData];
     await new Promise((resolve) => {
       envRegistry.environments$({}).subscribe(
         (data) => receivedEnvs.push(data),
@@ -40,7 +37,7 @@ describe('Environments$ test suite', () => {
         }
       );
     });
-    await envRegistry.register({ envKey: 'tag-2', env: environments['tag-2'] });
+    await envRegistry.register(tag2Data);
     receivedEnvs.length = 0;
     envRegistry.environments$({}).subscribe(
       (data) => receivedEnvs.push(data),
@@ -51,5 +48,36 @@ describe('Environments$ test suite', () => {
         done();
       }
     );
+  });
+
+  it('Subscribe to environments$ method returns all available envKeys and Envs (httpFile configProvider)', (done) => {
+    expect.assertions(2);
+
+    // @ts-ignore
+    global.fetch = jest.fn((fetchUrl) => {
+      expect(fetchUrl).toBe('http://localhost:3000/env-repo.json');
+      return Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            develop: environments.develop,
+            master: environments.master,
+          }),
+      });
+    });
+
+    const envRegistryHttpFile = new EnvRegistry<Env>({
+      token: 'http://localhost:3000',
+      configProvider: 'httpFile',
+      repository: 'env-repo',
+    });
+    envRegistryHttpFile
+      .environments$({})
+      .pipe(toArray())
+      .subscribe((envs) => {
+        expect(envs).toEqual([devData, masterData]);
+        // @ts-ignore
+        global.fetch.mockClear();
+        done();
+      });
   });
 });
